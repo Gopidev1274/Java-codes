677500d9c680492284f2f903dfdb455a86034b00
Gopidev1274 <gopigamer.1274@gmail.com>
2019-09-18 09:55:35 +0530

    success

---
+++ src/org/day1/OddorEven.java
@@ -0,0 +1,13 @@
+package org.day1;
+
+public class OddorEven {
+public static void main(String[] args) {
+	int i=0;
+	if(i%2==0) {
+		System.out.println("Even number");
+	}
+	else {
+		System.out.println("Odd number");
+	}
+}
+}
